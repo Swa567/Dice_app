906db8bc3f323aa31f91d4146e1282ab42edede0
Sarah <dousse.sarah@gmail.com>
2021-11-16 19:57:39 +0000

    refactoring

--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -1,10 +1,15 @@
 require 'dice'
 
+#Arrange, Act, Assert
+    #Arrange: getting everything we need for our test together
+    #Act: Running the code we have to run in order t test what we're getting
+    #Assert: Run our expect to check that our assumptions match the actual behavior
+
 def dice_number
     $stdin.gets.chomp
 end
 
-describe 'dice_number' do
+describe 'dice_number' do                                   # user story 2 => get user's input
     before do
       $stdin = StringIO.new("1")
     end
@@ -13,29 +18,33 @@ describe 'dice_number' do
         $stdin = STDIN
     end
 
-    it "should be '1'" do
+    it "should be '1'" do                                   # user story 1
         expect(dice_number).to be == "1"
     end
 end
 
 describe Dice do
-    it "allows the user to roll a dice" do
-        #Arrange, Act, Assert
-        #Arrange: getting everything we need for our test together
-        #Act: Running the code we have to run in order t test what we're getting
-        #Assert: Run our expect to check that our assumptions match the actual behavior
+    it "allows the user to roll a dice" do                  # user story 1 => dice respond to roll
         expect(subject).to respond_to(:roll)
-        #Subject is the same as creating dice = Dice.new
+        # Subject is the same as creating dice = Dice.new
     end
+
     # xit "returns a number between one and six" do
     #     dice = Dice.new
     #     expect(dice.roll).to eq(1)
     # end
-    it "the dice roll should be random between 1 and 6" do
+
+    it "returns a random number between 1 and 6" do
+        # expect a number bigger than 1
         expect(subject.roll).to be >= 1
-        expect(subject.roll). to be <= 6
+        # expect a number smaller than 6
+        expect(subject.roll).to be <= 6
+        # check it is an integer
+        expect(subject.roll).to be_an(Integer)
     end
-    it "User is able to input info" do
-        expect($stdin).to respond_to(:gets)
+
+    it "allows user to roll several dices" do
+
     end
+
 end
